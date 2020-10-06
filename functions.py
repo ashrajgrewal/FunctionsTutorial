2461bf5f15c0071a72ec8874e6a0785129ebd8ab
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-10-05 19:05:41 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,27 @@
+#Ashraj Grewal CS1.0 Functions Tutorial
+#Q1
+def area(width, height):
+    result = width * height
+    return result
+#Q2
+result = area(5, 6)
+print(result)
+#If we commented out print we would see nothing, because it wouldn't display result.
+#Q3
+result2 = area(4, 4)
+print (result2)
+result3 = area(9, 9)
+print (result3)
+#Q4
+#___ subtract(num1, num2):
+ #   ___ = num1 - num2
+ #   ___ result
+ #To complete it we would add def, result, and return
+def subtract(num1, num2):
+     result = num1 - num2
+     return result
+#Q5
+result4 = subtract(5, 5)
+print(result4)
+result5 = subtract(10, 9)
+print(result5)
